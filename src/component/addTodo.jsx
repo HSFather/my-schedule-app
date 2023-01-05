57747faba506047b9a9289647a8f1de5eba76c83
HSFather <99577466+HSFather@users.noreply.github.com>
2023-01-05 15:43:50 +0900

    add calendar

--- src/component/addTodo.jsx
+++ src/component/addTodo.jsx
@@ -1,11 +1,13 @@
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect, useState } from "react";
+import { Calendar } from "react-calendar";
 import Modal from "./modal";
 
 const AddTodo = ({ addTodo }) => {
   const [value, setValue] = useState("");
   const [modalOpen, setModalOpen] = useState(false);
+  const [date, setDate] = useState(new Date());
 
   const openModal = () => {
     setModalOpen(true);
@@ -35,8 +37,22 @@ const AddTodo = ({ addTodo }) => {
             <FontAwesomeIcon icon={faPlus} />
           </button>
           <Modal open={modalOpen} close={closeModal} header="스케쥴 추가하기">
-            <div>인풋</div>
-            <div>달력</div>
+            <form onSubmit={handleSubmit}>
+              <div className="modal_input">
+                <label htmlFor="">해야 할 일</label>
+                <input
+                  type="text"
+                  onChange={(e) => setValue(e.target.value)}
+                  placeholder="please add task"
+                />
+                <button type="submit" disabled={value.length < 1}>
+                  Submit
+                </button>
+              </div>
+              <div className="calendar">
+                <Calendar onChange={setDate} value={date} />
+              </div>
+            </form>
           </Modal>
         </div>
       </form>
